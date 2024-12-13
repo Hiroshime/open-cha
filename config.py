af698495e11700f4fef6121b0b159f8448601fb5
Hiroshime <luizantonio1127@outlook.com>
2024-12-13 15:39:03 -0300

    new user page and database connection

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///scheduler.db'
-# os.getenv('DB_STRING')
+SQLALCHEMY_DATABASE_URI = os.getenv('DB_STRING')
+#'sqlite:///scheduler.db'
